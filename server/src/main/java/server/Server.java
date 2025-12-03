45ff33d066ebe0a22012182d79792e0dd9fedf8f
Your NameSam <you@example.comsamuelsethchristensen3@gmail.com>
2025-12-03 15:59:24 -0700

    final exception code edit

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -245,7 +245,7 @@ public class Server {
             case "Error: bad request" -> 400;
             case "Error: unauthorized" -> 401;
             case "Error: already taken" -> 403;
-            case "Error: Unauthorize" -> 401;
+            case "Error: Unauthorized" -> 401;
             default -> 500;
         };
         // there are some dataAccessExceptions that get raised where I should be including ServiceExceptions instead. Found in game and user services respectively.
