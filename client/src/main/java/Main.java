0e5b58696323e1fce1559581a620004bb076b8c7
Your NameSam <you@example.comsamuelsethchristensen3@gmail.com>
2025-12-08 14:59:42 -0700

    Updated main to print to console

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,27 @@
+//import chess.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Client: " + piece);
+//    }
+//}
+
+
 import chess.*;
+import client.ChessClient;
+import client.ServerFacade;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        int port = 8081; // or read from args/config to match your Server.run(...)
+        String baseUrl = "http://localhost:" + port;
+
+        ServerFacade facade = new ServerFacade(baseUrl);
+        ChessClient client = new ChessClient(facade);
+        client.run();
     }
 }
