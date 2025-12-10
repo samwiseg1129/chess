cc7daee0379432f207967cb6802cf9a4cbd3c4cc
Your NameSam <you@example.comsamuelsethchristensen3@gmail.com>
2025-12-09 20:29:51 -0700

    Server Facade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,15 +1,18 @@
 package client;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.sun.net.httpserver.Request;
 import model.AuthData;
 import model.GameData;
 import model.GameList;
 
 import java.io.*;
-import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +20,7 @@ import java.util.Map;
 public class ServerFacade {
     private final String baseUrl;
     private final Gson gson = new Gson();
+    private final HttpClient client = HttpClient.newHttpClient();
 
     public ServerFacade(String baseUrl) {
         this.baseUrl = baseUrl;
@@ -25,14 +29,14 @@ public class ServerFacade {
     public AuthData register(String username, String password, String email) throws Exception {
         var body = Map.of("username", username, "password", password, "email", email);
         String json = gson.toJson(body);
-        String response = sendRequest("POST", "/user", null, json);
+        String response = handleResponse(sendRequest("POST", "/user", null, json));
         return gson.fromJson(response, AuthData.class);
     }
 
     public AuthData login(String username, String password) throws Exception {
         var body = Map.of("username", username, "password", password);
         String json = gson.toJson(body);
-        String response = sendRequest("POST", "/session", null, json);
+        String response = handleResponse(sendRequest("POST", "/session", null, json));
         return gson.fromJson(response, AuthData.class);
     }
 
@@ -43,21 +47,21 @@ public class ServerFacade {
     public GameData createGame(String authToken, String gameName) throws Exception {
         var body = Map.of("gameName", gameName);
         String json = gson.toJson(body);
-        String response = sendRequest("POST", "/game", authToken, json);
+        String response = handleResponse(sendRequest("POST", "/game", authToken, json));
 
         return gson.fromJson(response, GameData.class);
     }
 
     public List<GameData> listGames(String authToken) throws Exception {
-        String response = sendRequest("GET", "/game", authToken, null);
+        String response = handleResponse(sendRequest("GET", "/game", authToken, null));
         GameList g = gson.fromJson(response, GameList.class);
         return g.games();
     }
 
-    public GameData joinGame(String authToken, int gameId, String colorOrNull) throws Exception {
-        var body = Map.of("gameId", gameId, "playerColor", colorOrNull);
+    public GameData joinGame(String authToken, int gameId, String color) throws Exception {
+        var body = Map.of("gameID", gameId, "playerColor", color);
         String json = gson.toJson(body);
-        String response = sendRequest("PUT", "/game", authToken, json);
+        String response = handleResponse(sendRequest("PUT", "/game", authToken, json));
         return gson.fromJson(response, GameData.class);
     }
 
@@ -66,46 +70,32 @@ public class ServerFacade {
     }
 
 
-    private String sendRequest(String method, String path, String authToken, String requestBody) throws Exception {
-        URL url = new URL(baseUrl + path);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod(method);
-        connection.setDoInput(true);
-        connection.setRequestProperty("Accept", "application/json");
-
-        if (authToken != null) {
-            connection.setRequestProperty("Authorization", authToken);
+    private HttpResponse<String> sendRequest(String method, String path, String authToken, String requestBody) throws Exception {
+        HttpRequest.Builder requestBuilder = null;
+        if (requestBody == null) {
+            requestBuilder = HttpRequest.newBuilder().uri(URI.create(baseUrl + path)).method(method, HttpRequest.BodyPublishers.noBody());
+        } else {
+            requestBuilder = HttpRequest.newBuilder().uri(URI.create(baseUrl + path)).method(method, HttpRequest.BodyPublishers.ofString(requestBody));
         }
-
         if (requestBody != null) {
-            connection.setDoOutput(true);
-            connection.setRequestProperty("Content-Type", "application/json");
-            try (OutputStream os = connection.getOutputStream()) {
-                byte[] input = requestBody.getBytes(StandardCharsets.UTF_8);
-                os.write(input);
-            }
+            requestBuilder.setHeader("Content-Type", "application/json");
         }
-
-        int status = connection.getResponseCode();
-
-        InputStream stream = (status >= 200 && status < 300)
-                ? connection.getInputStream() : connection.getErrorStream();
-
-        StringBuilder response = new StringBuilder();
-        if (stream != null) {
-            try (BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-            }
+        if (authToken != null) {
+            requestBuilder.setHeader("authorization", authToken);
         }
+        HttpRequest request = requestBuilder.build();
 
-        if (status < 200 || status >= 300) {
-            throw new RuntimeException("HTTP " + status + ": " + response);
-        }
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        return response.toString();
     }
+    private String handleResponse (HttpResponse<String> response) throws ErrorResult {
+        var status = response.statusCode();
+        if (status == 200) {
+            return response.body();
+        }
+        ErrorResult handleError = gson.fromJson(response.body(), ErrorResult.class);
+        throw (handleError);
+    }
+
+
 }
