a8be8e9c2bc4347c80e0db7982d462bb5290d73d
Your NameSam <you@example.comsamuelsethchristensen3@gmail.com>
2025-12-10 12:07:17 -0700

    WebsocketCommunicator in client

--- client/src/main/java/client/websocket/WebsocketCommunicator.java
+++ client/src/main/java/client/websocket/WebsocketCommunicator.java
@@ -1,70 +1,69 @@
 package client.websocket;
 
-public class WebsocketCommunicator {
-}
+import com.google.gson.Gson;
+import exception.ResponseException;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
 
-//package client.websocket;
-//
-//import com.google.gson.Gson;
-//import exception.ResponseException;
-//import webSocketMessages.Action;
-//import webSocketMessages.Notification;
-//
-//import jakarta.websocket.*;
-//
-//        import java.io.IOException;
-//import java.net.URI;
-//import java.net.URISyntaxException;
-//
-////need to extend Endpoint for websocket to work properly
-//public class WebSocketFacade extends Endpoint {
-//
-//    Session session;
-//    NotificationHandler notificationHandler;
-//
-//    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
-//        try {
-//            url = url.replace("http", "ws");
-//            URI socketURI = new URI(url + "/ws");
-//            this.notificationHandler = notificationHandler;
-//
-//            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-//            this.session = container.connectToServer(this, socketURI);
-//
-//            //set message handler
-//            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
-//                @Override
-//                public void onMessage(String message) {
-//                    Notification notification = new Gson().fromJson(message, Notification.class);
-//                    notificationHandler.notify(notification);
-//                }
-//            });
-//        } catch (DeploymentException | IOException | URISyntaxException ex) {
-//            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
-//        }
-//    }
-//
-//    //Endpoint requires this method, but you don't have to do anything
-//    @Override
-//    public void onOpen(Session session, EndpointConfig endpointConfig) {
-//    }
-//
-//    public void enterPetShop(String visitorName) throws ResponseException {
-//        try {
-//            var action = new Action(Action.Type.ENTER, visitorName);
-//            this.session.getBasicRemote().sendText(new Gson().toJson(action));
-//        } catch (IOException ex) {
-//            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
-//        }
-//    }
-//
-//    public void leavePetShop(String visitorName) throws ResponseException {
-//        try {
-//            var action = new Action(Action.Type.EXIT, visitorName);
-//            this.session.getBasicRemote().sendText(new Gson().toJson(action));
-//        } catch (IOException ex) {
-//            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
-//        }
-//    }
-//
-//}
+import jakarta.websocket.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebsocketCommunicator extends Endpoint {
+
+    private Session session;
+    private final Gson gson = new Gson();
+    private final NotificationHandler notificationHandler;
+
+    public WebsocketCommunicator(String baseUrl, NotificationHandler notificationHandler) throws ResponseException {
+        try {
+            String wsUrl = baseUrl.replace("http", "ws") + "/ws";
+            URI socketURI = new URI(wsUrl);
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
+                ServerMessage serverMessage = gson.fromJson(message, ServerMessage.class);
+                notificationHandler.notify(serverMessage);
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(ResponseException.Code.SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {}
+
+    private void sendCommand(UserGameCommand command) throws ResponseException {
+        try {
+            String json = gson.toJson(command);
+            session.getBasicRemote().sendText(json);
+        } catch (IOException ex) {
+            throw new ResponseException(ResponseException.Code.SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    public void connect(String authToken, int gameId) throws ResponseException {
+        var cmd = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameId);
+        sendCommand(cmd);
+    }
+
+    public void makeMove(String authToken, int gameId, chess.ChessMove move) throws ResponseException {
+        var cmd = new UserGameCommand(UserGameCommand.CommandType.MAKE_MOVE, authToken, gameId, move);
+        sendCommand(cmd);
+    }
+
+    public void leave(String authToken, int gameId) throws ResponseException {
+        var cmd = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameId);
+        sendCommand(cmd);
+    }
+
+    public void resign(String authToken, int gameId) throws ResponseException {
+        var cmd = new UserGameCommand(UserGameCommand.CommandType.RESIGN, authToken, gameId);
+        sendCommand(cmd);
+    }
+}
